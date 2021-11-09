e7bfe458241b97eb1c2b4c118d99c718831d67b4
Naoufel <naoufel.maazouzi@live.fr>
2021-11-09 16:43:38 +0100

    modify test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './redux/rootReducer';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+
+let store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+      </Provider>);
   const linkElement = getByText(/actions/i);
   expect(linkElement).toBeInTheDocument();
 });
